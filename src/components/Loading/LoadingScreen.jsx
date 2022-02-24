c684f21a42dbade536a7f56349f0d18e22aa3360
jcpoulson <jonahpoulson@gmail.com>
2022-02-24 15:28:28 -0500

    Style fixes

--- src/components/Loading/LoadingScreen.jsx
+++ src/components/Loading/LoadingScreen.jsx
@@ -3,6 +3,9 @@ import Counter from "../utils/Counter";
 import { motion } from "framer-motion";
 import { useNavigate } from "react-router-dom";
 
+import prsnaLogo from "../../assets/img/prsna-logo-full-color.svg";
+import loadingBar from "../../assets/img/loading-bar.svg";
+
 import "./styleguide.css";
 import "./style.css";
 
@@ -20,14 +23,14 @@ const LoadingScreen = () => {
     <motion.div initial={{opacity: 0 }} animate={{opacity: 1 }} exit={{opacity: 0 }} transition={{ duration: 1 }}>
       <div className="container-center-horizontal">
         <div className="prsnastudiowebsiteloadingscreendesktop screen" >
-          <img className="prsna_-logo_loading-screen" alt="prsna-logo" src="https://anima-uploads.s3.amazonaws.com/projects/6215578908245f0a14cf0140/releases/62155a0fd98caca3fa3450ab/img/prsna-logo-loading-screen-2@2x.png" />
+          <img className="prsna_-logo_loading-screen" alt="prsna-logo" src={prsnaLogo} />
           <div className="loading-bar" >
             <motion.img layout
               animate={{ width: "80%" }}
               transition={ { duration: 5 }}
               className="group-44"
               alt="loading-bar"  
-              src="https://anima-uploads.s3.amazonaws.com/projects/6215578908245f0a14cf0140/releases/62155a0fd98caca3fa3450ab/img/group-44-2@2x.png" 
+              src={loadingBar} 
               />
             <div className="percent nbarchitekt-regular-normal-thunderbird-20px" >
               <Counter from={0} to={100} />
